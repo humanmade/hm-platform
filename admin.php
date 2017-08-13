39787f280565929b2280699cf13be80fbb1b3b17
Joe Hoyle <joehoyle@gmail.com>
2017-08-13 19:01:22 -0400

    Remove the Custom meta box on edit post screen.
    Fixes #14.

--- admin.php
+++ admin.php
@@ -167,3 +167,14 @@ function get_platform_actions( $actions, $plugin_file, $plugin_data, $context ) 
 
 	return $actions;
 }
+
+/**
+ * Disable the Custom Meta box on the post edit screen.
+ *
+ * This meta box is very bad for performance due to the `postmeta_form_keys` query
+ * and how it counts all the post meta keys. It's a useless box, so better to just
+ * not use it.
+ */
+add_action( 'add_meta_boxes', function () {
+	remove_meta_box( 'postcustom', null, 'normal' );
+});
