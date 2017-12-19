59f07802d8764e02c13cd7ca35f95ba80986176e
mikeselander <mikeselander@humanmade.co.uk>
2017-12-19 12:57:32 -0800

    Removed default stories and added placeholders for our story sets

---
+++ src/stories/admin.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+/**
+ * Stories for Admin-side generic components.
+ */
+storiesOf( 'Admin', module )
+	.add( 'Placeholder', () => <p>Hello World</p> );
