d39977e2f76a3f61bcbc2951ee3bd9c02c0e7fd0
mikeselander <mikeselander@humanmade.co.uk>
2017-12-19 14:19:30 -0800

    Add our application's CSS

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
 import { configure } from '@storybook/react';
 
+// Our Application's CSS.
+import '../src/index.css';
+
 const req = require.context('../src/stories', true, /\.stories\.js$/)
 
 function loadStories() {
