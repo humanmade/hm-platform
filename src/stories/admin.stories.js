c943391ba89545c6563dfb6f32241eb0ecdf0600
mikeselander <mikeselander@humanmade.co.uk>
2017-12-20 10:33:36 -0800

    Added basic decorator to give an admin block the appropriate dashboard class/ID wrappers

--- src/stories/admin.stories.js
+++ src/stories/admin.stories.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
+import { DashboardAdminDecorator } from './decorators';
 import DashboardBlock from '../components/Dashboard-Block'
 
 /**
  * Stories for Admin-side generic components.
  */
 storiesOf( 'Admin', module )
+	.addDecorator( DashboardAdminDecorator )
 	.add( 'Dashboard Block', () => <div id="dashboard-widgets"><DashboardBlock title={ 'My Little Dashboard Block' } /></div> );
--- src/stories/cloud.stories.js
+++ src/stories/cloud.stories.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
+import DashboardBlock from '../components/Dashboard-Block'
+
 /**
  * Stories for HM-Cloud UI components.
  */
 storiesOf( 'Cloud', module )
-	.add( 'Placeholder', () => <p>Hello World</p> );
+	.add( 'Dashboard Block', () => <DashboardBlock title={ 'My Little Dashboard Block' } /> );
