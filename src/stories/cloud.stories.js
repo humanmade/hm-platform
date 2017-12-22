829abd9fc18a4e8f81ac127af4a4487c38362024
mikeselander <mikeselander@humanmade.co.uk>
2017-12-21 16:09:32 -0800

    Added stories for the Alerts Log and Alert Item components

--- src/stories/cloud.stories.js
+++ src/stories/cloud.stories.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
+import AlertsLog from '../components/Cloud/Alerts-Log'
 import PullRequests from '../components/Cloud/Pull-Requests'
 import { DashboardAdminDecorator } from './decorators';
 
-import { pullRequests } from './cloud-data-stubs';
+import {
+	alerts,
+	pullRequests,
+} from './cloud-data-stubs';
 
 /**
  * Stories for HM-Cloud UI components.
  */
 storiesOf( 'Cloud', module )
 	.addDecorator( DashboardAdminDecorator )
+	.add( 'Activity Log', () => <AlertsLog items={ alerts } /> )
 	.add( 'Pull Requests', () => <PullRequests items={ pullRequests } /> );
