fc99d5a5c3a46ffcaf67007bac53211414e635c8
mikeselander <mikeselander@humanmade.co.uk>
2018-01-08 11:07:30 -0800

    Use italic styles and datetime in date tag

--- src/components/Cloud/Alert-Item.js
+++ src/components/Cloud/Alert-Item.js
@@ -6,13 +6,15 @@ import { getTimeSince } from '../../utils';
 /**
  * Display status about a single Pull Request.
  *
- * @param {String} date       Date that the pull request was opened.
+ * @param {String} date    Date that the pull request was opened.
+ * @param {String} level   Level of intensity of the alert.
+ * @param {String} message Alert message to display.
  */
 const AlertItem = ( { date, level, message } ) => {
 	const parsedDate = new Date( date );
 	return <li className={ `alert-item alert-item--${ level }` }>
 		<p className='alert-item__message'>{ message }</p>
-		<time className="alert-item__time"><i>Updated { getTimeSince( date ) }</i></time>
+		<time datetime={ parsedDate.toISOString() } className="alert-item__time">Updated { getTimeSince( date ) }</time>
 	</li>
 }
 
