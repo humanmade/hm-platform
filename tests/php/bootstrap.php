23116e2f21a16b4df021256af24ec6e3292a0571
Fränk Klein <frank@humanmade.co.uk>
2018-01-19 17:13:27 +0100

    Add basic testing setup.

---
+++ tests/php/bootstrap.php
@@ -0,0 +1,74 @@
+<?php
+// @codingStandardsIgnoreFile
+
+/**
+ * If the tests need any kind of setup before running, this should be done in this file.
+ *
+ * @package techcrunch-2017
+ */
+
+// On Chassis, tests can silently fail, so introduce a shutdown function to print the last error.
+// Throwing an exception sends a non-zero exit code.
+register_shutdown_function( function() {
+	// Only load in Chassis.
+	if ( ! defined( 'WP_LOCAL_DEV' ) ) {
+		return;
+	}
+
+	$error = error_get_last();
+	if ( $error && isset( $error['message'] ) && ! defined( 'DOING_AJAX' ) ) {
+		throw new Exception( $error['message'] );
+	}
+} );
+
+$wp_tests_dir = getenv( 'WP_TESTS_DIR' );
+$wp_develop_dir = getenv( 'WP_DEVELOP_DIR' );
+
+require_once $wp_tests_dir . '/includes/functions.php';
+
+/*
+function _register_theme() {
+
+	$theme_dir = dirname( dirname( dirname( __FILE__ ) ) );
+	$current_theme = basename( $theme_dir );
+
+	add_filter( 'pre_option_template', function() use ( $current_theme ) {
+		return $current_theme;
+	});
+	add_filter( 'pre_option_stylesheet', function() use ( $current_theme ) {
+		return $current_theme;
+	});
+
+	// Set our permalink structure to always match production in tests.
+	add_filter( 'pre_option_permalink_structure', function() {
+		return '%year%/%monthnum%/%day%/%postname%';
+	}, 99 );
+}
+tests_add_filter( 'muplugins_loaded', '_register_theme' );
+
+tests_add_filter( 'setup_theme', function () {
+	require __DIR__ . '/includes/class-mockuser.php';
+} );
+*/
+
+
+/**
+ * Re-map the default `/uploads` folder with our own `/test-uploads` for tests.
+ *
+ * WordPress core runs a method (scan_user_uploads) on the first instance of `WP_UnitTestCase`.
+ * This method scans every single folder and file in the uploads directory. This is a problem
+ * as uploads from the last year total well over 36Gb to start off and scanning that takes over
+ * 5 minutes.
+ *
+ * This filter adds a unique test uploads folder just for our tests to reduce load.
+ */
+tests_add_filter( 'upload_dir', function( $dir ) {
+	array_walk( $dir, function( &$item ) {
+		if ( is_string( $item ) ) {
+			$item = str_replace( '/uploads', '/test-uploads', $item );
+		}
+	} );
+	return $dir;
+} );
+
+require_once $wp_tests_dir . '/includes/bootstrap.php';
---
+++ tests/php/tests/sample-test.php
@@ -0,0 +1,7 @@
+<?php
+class JSON_LD_Test extends WP_UnitTestCase {
+	public function test_something() {
+		$this->assertFalse( false );
+	}
+
+}
