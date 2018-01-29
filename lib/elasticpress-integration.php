f4489ba0823f1c4fc7858ff57822b5e1de3f9eae
Robert O'Rourke <rob@hmn.md>
2018-01-29 15:11:36 +0000

    Don't sign requests on local

--- lib/elasticpress-integration.php
+++ lib/elasticpress-integration.php
@@ -10,6 +10,9 @@ function bootstrap() {
 	if ( ! defined( 'ELASTICSEARCH_HOST' ) ) {
 		return;
 	}
+	if ( defined( 'HM_ENV_TYPE' ) && HM_ENV_TYPE === 'local' ) {
+		return;
+	}
 	define( 'EP_HOST', ELASTICSEARCH_HOST );
 	add_filter( 'http_request_args', __NAMESPACE__ . '\\on_http_request_args', 10, 2 );
 	add_filter( 'ep_pre_request_url', function( $url ) {
