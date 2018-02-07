b8d58aa8cbc32a78324aa94b5e6d13481b7b48ac
Robert O'Rourke <robert@o-rourke.org>
2018-02-07 18:56:19 +0000

    force comment pagination

--- plugins/performance/performance.php
+++ plugins/performance/performance.php
@@ -16,3 +16,18 @@
 add_action( 'add_meta_boxes', function () {
 	remove_meta_box( 'postcustom', null, 'normal' );
 } );
+
+/**
+ * Force comment pagination.
+ *
+ * Posts with a lot of comments are slow to generate and can result in
+ * poor performance and in some cases make can a site unresponsive.
+ */
+add_filter( 'pre_option_page_comments', '__return_true' );
+add_filter( 'option_comments_per_page', function ( $value ) {
+	if ( empty( $value ) || intval( $value ) > 50 ) {
+		return 50;
+	}
+
+	return $value;
+} );
