021e60b4949770de3a98a656efe12796d079510e
Joe Hoyle <joehoyle@gmail.com>
2018-11-12 22:07:50 +0000

    Remove error_log

--- inc/class-db.php
+++ inc/class-db.php
@@ -8,7 +8,6 @@ class DB extends LudicrousDB {
 	function query( $query ) {
 		$start = microtime( true );
 		$result = parent::query( $query );
-		//	error_log( $this->last_connection['host'] );
 		if ( function_exists( __NAMESPACE__ . '\\XRay\\trace_wpdb_query' ) ) {
 			$host = $this->current_host ?: $this->last_connection['host'];
 			// Host gets the port number applied, which we don't want to add.
