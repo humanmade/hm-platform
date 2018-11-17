5b91a591c0a98bc72f9d75004d0dd347b1afccfb
Nathaniel Schweinberg <natanielks@gmail.com>
2018-11-16 17:30:00 -0800

    When using Redis, clear the alloptions cache whenever an option is added/updated/deleted

---
+++ inc/redis_alloptions/namespace.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace HM\Platform\Redis_Alloptions;
+
+function bootstrap() {
+	add_action( 'added_option', __NAMESPACE__ . '\maybe_clear_alloptions_cache' );
+	add_action( 'updated_option', __NAMESPACE__ . '\maybe_clear_alloptions_cache' );
+	add_action( 'deleted_option', __NAMESPACE__ . '\maybe_clear_alloptions_cache' );
+}
+
+/**
+ * Fix a race condition in alloptions caching
+ *
+ * See https://core.trac.wordpress.org/ticket/31245#comment:57
+ */
+function maybe_clear_alloptions_cache( $option ) {
+	if ( ! wp_installing() ) {
+		$alloptions = wp_load_alloptions(); //alloptions should be cached at this point
+
+		if ( isset( $alloptions[ $option ] ) ) { //only if option is among alloptions
+			wp_cache_delete( 'alloptions', 'options' );
+		}
+	}
+}
--- load.php
+++ load.php
@@ -126,11 +126,15 @@ function load_object_cache() {
 	}
 
 	wp_using_ext_object_cache( true );
-	if( $config['memcached'] ) {
+	if ( $config['memcached'] ) {
 		require __DIR__ . '/dropins/wordpress-pecl-memcached-object-cache/object-cache.php';
-	} else if ( $config['redis'] ) {
+	} elseif ( $config['redis'] ) {
+		require __DIR__ . '/inc/redis_alloptions/namespace.php';
 		require __DIR__ . '/dropins/wp-redis-predis-client/vendor/autoload.php';
+
+		Redis_Alloptions\bootstrap();
 		\WP_Predis\add_filters();
+
 		require __DIR__ . '/plugins/wp-redis/object-cache.php';
 	}
 
