f879d8b50cc2a3d1b512a07c3d3053ca22095405
Nathaniel Schweinberg <natanielks@gmail.com>
2019-01-23 13:20:55 -0800

    update xray, remove reference to is_bootstraped()

--- inc/class-db.php
+++ inc/class-db.php
@@ -9,7 +9,7 @@ class DB extends LudicrousDB {
 	function query( $query ) {
 		$start = microtime( true );
 		$result = parent::query( $query );
-		if ( function_exists( __NAMESPACE__ . '\\XRay\\is_bootstrapped' ) && XRay\is_bootstrapped() ) {
+		if ( function_exists( __NAMESPACE__ . '\\XRay\\trace_wpdb_query' ) ) {
 			$host = $this->current_host ?: $this->last_connection['host'];
 			// Host gets the port number applied, which we don't want to add.
 			$host = strtok( $host, ':' );
